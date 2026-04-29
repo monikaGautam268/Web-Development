6d60ebe9b973d0c7bea1ed5503974844141fe9d2
monikaGautam268 <mg461717@gmail.com>
2026-04-29 19:23:34 +0530

    LoopInJavascript

---
+++ JSBasic/loop.js
@@ -0,0 +1,37 @@
+console.log("hellow");
+// var n = 10;
+// for (var i = 1; i <= n; i++) {
+//   console.log(i * 2);
+// }
+ 
+
+let a=[24,58,33,86,96,12,53,23,32,100]
+// for( var i=0;i<10;i++)
+// {
+//     console.log(a[i])
+// }
+
+// let i=0;
+// while(i<10)
+// {
+//     console.log(a[i])
+//     i++
+// }
+
+
+// while table of 2
+var i=1;
+while(i<=10)
+{
+    console.log(i*2)
+    i++ 
+}
+
+
+
+// do while table of 2
+var i=1;
+do{
+    console.log(i*2)
+    i++
+}while (i<=10);
